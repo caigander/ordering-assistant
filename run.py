f678c436a03b62219e08b5921fd96e4323eb5490
caigander <caigander@gmail.com>
2018-11-25 18:06:09 -0700

    Both Vendors working, certificate cancellation needs user click

--- run.py
+++ run.py
@@ -1,40 +1,84 @@
 
 import time
-import os
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from login import o_user, o_pass, o_url, m_url, page_user_id, page_pass_id, page_butn_id, webdriver_path
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from login import m_user, z_user, m_pass, z_pass, m_url, z_url, page_user_id, page_pass_id, page_butn_id, webdriver_path
 
-browser = webdriver.Chrome(webdriver_path)
-browser.get(o_url)
+def get_muir():
+    browser = webdriver.Chrome(webdriver_path)
+    browser.get(m_url)
 
-#Find element of login name and send credentials to element
-userid = browser.find_element_by_id(page_user_id)
-userid.send_keys(o_user)
+    #Find element of login name and send credentials to element
+    userid = browser.find_element_by_id(page_user_id)
+    userid.send_keys(m_user)
 
-#Find element of password and send credentials to find_element_by_id
-psw = browser.find_element_by_id(page_pass_id)
-psw.send_keys(o_pass)
+    #Find element of password and send credentials to find_element_by_id
+    psw = browser.find_element_by_id(page_pass_id)
+    psw.send_keys(m_pass)
 
-#Find element of button and click it
-button = browser.find_element_by_id(page_butn_id)
-button.click()
+    #Find element of button and click it
+    button = browser.find_element_by_id(page_butn_id)
+    button.click()
 
-time.sleep(3)
-# Pulls html down as variable
-page = browser.page_source
+    time.sleep(1)
 
-# Returns list of all tables on page
-tables = pd.read_html(page)
+    # Load page source and return list of all tables on page
+    muir_tables = pd.read_html(browser.page_source)
 
-# Selects table of produce
-items = tables[5]
-#items.drop([1,2,3,5], 1, inplace = True)
+    # Selects table of produce
+    muir_items = muir_tables[5]
 
-items.to_csv("muir.csv", sep='\t', index = False)
+    #Add new column to dataframe
+    #muir_items[6] = ''
+    muir_items.to_csv("muir.csv", sep='\t', index = False)
+    browser.close()
+
+
+    #items.drop([1,2,3,5], 1, inplace = True)
+    #items.to_csv("muir.csv", sep='\t', index = False)
+    #item_list = items[0].tolist()
+def get_az():
+    browser = webdriver.Chrome(webdriver_path)
+    browser.get(z_url)
+
+    #Find element of login name and send credentials to element
+    userid = browser.find_element_by_id(page_user_id)
+    userid.send_keys(z_user)
+
+    #Find element of password and send credentials to find_element_by_id
+    psw = browser.find_element_by_id(page_pass_id)
+    psw.send_keys(z_pass)
+
+    #Find element of button and click it
+    button = browser.find_element_by_id(page_butn_id)
+    button.click()
+
+    time.sleep(1)
+
+    #Select full menu
+    menu = browser.find_element_by_id('ctl00_CPH2_grdMenu_ctl03_lnkPicker')
+    menu.click()
+
+    # Load page source and return list of all tables on page
+    az_tables = pd.read_html(browser.page_source)
+
+    # Selects table of produce
+    az_items = az_tables[5]
+
+    #Add new column to dataframe
+    #muir_items[6] = ''
+    az_items.to_csv("az.csv", sep='\t', index = False)
+    browser.close()
+
+
+    #items.drop([1,2,3,5], 1, inplace = True)
+    #items.to_csv("muir.csv", sep='\t', index = False)
+    #item_list = items[0].tolist()
+
+#get_muir()
+get_az()
